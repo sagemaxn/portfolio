de51496e443dde912e14eafa0a111cc2dfd6c43e
sagemaxn <sagemaxn@gmail.com>
2023-02-05 22:06:43 -0500

    dokku

--- backend/server.ts
+++ backend/server.ts
@@ -1,37 +1,41 @@
-import * as Express from "express";
-import "reflect-metadata";
-import { ApolloServer } from "apollo-server-express";
-import { buildSchema, UseMiddleware } from "type-graphql";
+import * as Express from 'express';
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 
-import {ContactResolver} from './src/Resolver'
+import { ContactResolver } from './src/Resolver';
 
-require("dotenv").config();
+import { config } from 'dotenv';
+
+config();
 
 const app = Express();
 
-
 const main = async () => {
     const schema = await buildSchema({
-      resolvers: [ContactResolver],
-      //emitSchemaFile: true
+        resolvers: [ContactResolver],
+        //emitSchemaFile: true
     });
     const apolloServer = new ApolloServer({
-      schema,
-      context: async ({ req, res }) => {
-        return { res, req, payload: "asd" };
-      },
+        schema,
+        context: async ({ req, res }) => {
+            return { res, req, payload: 'asd' };
+        },
     });
 
     await apolloServer.start();
-  
-    apolloServer.applyMiddleware({ app, cors: {
-      credentials: true,
-      methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-      origin: 'http://localhost:3000',
-    } });
-  
-    app.listen(4000, () => {
-      console.log("Server started on 4000");
+
+    apolloServer.applyMiddleware({
+        app,
+        cors: {
+            credentials: true,
+            methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+            origin: 'http://localhost:3000',
+        },
     });
-  };
-  main();
+
+    app.listen(4000, () => {
+        console.log('Server started on 4000');
+    });
+};
+main();
--- backend/src/Resolver.ts
+++ backend/src/Resolver.ts
@@ -1,91 +1,93 @@
-import { oauth2 } from "googleapis/build/src/apis/oauth2";
-import { Resolver, Query, Mutation, Arg, Ctx, InputType, Field, ObjectType } from "type-graphql";
+import {
+    Arg,
+    Field,
+    InputType,
+    Mutation,
+    ObjectType,
+    Query,
+    Resolver,
+} from 'type-graphql';
 
+import nodemailer from 'nodemailer';
+
+import { google } from 'googleapis';
+const OAuth2 = google.auth.OAuth2;
 @ObjectType()
 export class Message {
-  @Field(() => String)
-  public name: string;
+    @Field(() => String)
+    public name: string;
 
-  @Field(() => String)
-  public email: string;
+    @Field(() => String)
+    public email: string;
 
-  @Field(() => String)
-  public message: string
+    @Field(() => String)
+    public message: string;
 }
 
 @InputType()
 export class MessageInput {
     @Field(() => String)
-    name: string
+    name: string;
     @Field(() => String)
-    email: string
+    email: string;
     @Field(() => String)
-    message: string
+    message: string;
 }
-
-const nodemailer = require('nodemailer');
-const xoauth2 = require('xoauth2')
-const { google } = require('googleapis')
-const OAuth2 = google.auth.OAuth2
-
-
 
 @Resolver()
 export class ContactResolver {
-
-  @Query(() => String)
-  async test(){
-    const b = 'f'
-    return b
-  }
-
-  @Mutation(() => String)
-  async contactMessage(
-    @Arg('messageInput') { name, email, message } : MessageInput
-  ){
-    if(!name){
-      name = ''
+    @Query(() => String)
+    async test() {
+        const b: string;
+        b = 'f';
+        return b;
     }
-    var mailOptions = {
-      from: process.env.EMAIL,
-      to: process.env.EMAIL,
-      subject: `${name} ${email}`,
-      text: message
-    };
 
-  const oauth2Client = new OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
-  )
-  
-  oauth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN
-  })
-  const accessToken = oauth2Client.getAccessToken()
-  
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-            type: 'OAuth2',
-            user: 'sagemaxn@gmail.com',
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN,
-            accessToken: accessToken,
-  },
-  tls: {
-    rejectUnauthorized: false
-  }
-  });
+    @Mutation(() => String)
+    async contactMessage(
+        @Arg('messageInput') { name, email, message }: MessageInput,
+    ) {
+        if (!name) {
+            name = '';
+        }
+        const mailOptions = {
+            from: process.env.EMAIL,
+            to: process.env.EMAIL,
+            subject: `${name} ${email}`,
+            text: message,
+        };
 
-    transporter.sendMail(mailOptions, function(error, info){
-      error ? console.log(error) : console.log(info.response);
-      transporter.close();
-    });
+        const oauth2Client = new OAuth2(
+            process.env.CLIENT_ID,
+            process.env.CLIENT_SECRET,
+            'https://developers.google.com/oauthplayground',
+        );
 
-    return 's'
+        oauth2Client.setCredentials({
+            refresh_token: process.env.REFRESH_TOKEN,
+        });
+        const accessToken = oauth2Client.getAccessToken();
 
-  }
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: 'sagemaxn@gmail.com',
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                refreshToken: process.env.REFRESH_TOKEN,
+                accessToken: accessToken,
+            },
+            tls: {
+                rejectUnauthorized: false,
+            },
+        });
 
+        transporter.sendMail(mailOptions, function (error, info) {
+            error ? console.log(error) : console.log(info.response);
+            transporter.close();
+        });
+
+        return 's';
+    }
 }
